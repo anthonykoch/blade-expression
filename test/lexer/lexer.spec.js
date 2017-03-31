4ddcd51b32f990e5a41678649294ef7c374b46a7
anthonykoch <anthonykoch10@gmail.com>
2017-03-31 08:52:50 -0700

    Update lexer tests

--- test/lexer/lexer.spec.js
+++ test/lexer/lexer.spec.js
@@ -3,98 +3,103 @@
 const test = require('tape');
 
 const Lexer = require('../../lib/lexer');
-const DistLexer = require('../../dist/bladeexp.js').Lexer;
-const DistMinLexer = require('../../dist/bladeexp.min.js').Lexer;
 
-[Lexer, DistLexer, DistMinLexer].map(Lexer => {
-    test('Lexer - reduces input when using non-sticky regex', t => {
-        const data = `name + greeting`;
-        const lexer = Lexer.create(data);
-        lexer.nextToken();
-        t.notEqual(lexer.input, data);
-        t.end();
-    });
+test('Lexer - reduces input when using non-sticky regex', t => {
+    const data = `name + greeting`;
+    const lexer = Lexer.create(data);
+    lexer.nextToken();
+    t.notEqual(lexer.input, data);
+    t.end();
+});
 
-    test('Lexer - strips BOM', t => {
-        t.equals(Lexer.create('\uFEFFhello').input, 'hello');
-        t.notEquals(Lexer.create('hello\uFEFF'), 'hello\uFEFF',
-            'does not remove BOM ');
-        t.end();
-    });
+test('Lexer - strips BOM', t => {
+    t.equals(Lexer.create('\uFEFFhello').input, 'hello');
+    t.notEquals(Lexer.create('hello\uFEFF'), 'hello\uFEFF',
+        'does not remove BOM ');
+    t.end();
+});
 
-    test('Lexer.create - returns an object', t => {
-        const data = `name + greeting`;
-        const lexer = Lexer.create(data);
-        t.ok(lexer);
-        t.equal(typeof lexer, 'object');
-        t.throws(() => Lexer.create(), /of undefined/);
-        t.end();
-    });
+test('Lexer.create - returns an object', t => {
+    const data = `name + greeting`;
+    const lexer = Lexer.create(data);
+    t.ok(lexer);
+    t.equal(typeof lexer, 'object');
+    t.throws(() => Lexer.create(), /of undefined/);
+    t.end();
+});
 
-    test('lexer.nextToken', t => {
-        const data = `name + greeting`;
-        const lexer = Lexer.create(data);
+test('lexer.nextToken', t => {
+    const data = `name + greeting`;
+    const lexer = Lexer.create(data);
 
-        t.equals(lexer.nextToken().value, 'name',     'returns first token');
-        t.equals(lexer.nextToken().value, '+',        'returns second token');
-        t.equals(lexer.nextToken().value, 'greeting', 'returns third token');
-        t.equals(lexer.nextToken(),        null,      'end of tokens');
+    t.equals(lexer.nextToken().value, 'name',     'returns first token');
+    t.equals(lexer.nextToken().value, '+',        'returns second token');
+    t.equals(lexer.nextToken().value, 'greeting', 'returns third token');
+    t.equals(lexer.nextToken(),        null,      'end of tokens');
 
-        t.end();
-    });
+    t.end();
+});
 
-    test('lexer.skipWhitespace', t => {
-        const data = `     name     +     greeting     `;
-        const lexer = Lexer.create(data);
-        lexer.skipWhitespace();
-        t.equals(lexer.position, 5, 'skips whitespace')
-        t.end();
-    });
+test('lexer.skipWhitespace', t => {
+    const data = `     name     +     greeting     `;
+    const lexer = Lexer.create(data);
+    lexer.skipWhitespace();
+    t.equals(lexer.position, 5, 'skips whitespace')
+    t.end();
+});
 
-    test('lexer.lookahead', t => {
-        const data = `     name     +     greeting     `;
-        const lexer = Lexer.create(data);
+test('lexer.lookahead', t => {
+    const data = `     name     +     greeting     `;
+    const lexer = Lexer.create(data);
 
-        t.equal(lexer.lookahead(3).value, 'greeting', 'lookahead token');
-        t.equal(lexer.lookahead(4), null, 'null token');
+    t.equal(lexer.lookahead(3).value, 'greeting', 'lookahead token');
+    t.equal(lexer.lookahead(4), null, 'null token');
 
-        t.equals(lexer.stash[0].value, 'name',     'first token');
-        t.equals(lexer.stash[1].value, '+',        'second token');
-        t.equals(lexer.stash[2].value, 'greeting', 'third token');
+    t.equals(lexer.stash[0].value, 'name',     'first token');
+    t.equals(lexer.stash[1].value, '+',        'second token');
+    t.equals(lexer.stash[2].value, 'greeting', 'third token');
 
-        t.throws(() => lexer.lookahead(-1), /Lookahead index can not be less than 0/);
+    t.throws(() => lexer.lookahead(-1), /Lookahead index must be more than 0/);
+    t.throws(() => lexer.lookahead(0), /Lookahead index must be more than 0/);
 
-        t.end();
-    });
+    t.equals(
+        Lexer.create('apple juice').lookahead(1).value,
+        'apple',
+        'first lookahead returns first token');
 
-    test('lexer.peek', t => {
-        const data = `     name     +     greeting     `;
-        const lexer = Lexer.create(data);
+    t.end();
+});
 
-        t.equal(lexer.peek().value, 'name', 'peek token');
-        lexer.nextToken();
-        t.equal(lexer.peek().value, '+', 'peek token');
-        lexer.nextToken();
-        t.equal(lexer.peek().value, 'greeting', 'peek token');
-        lexer.nextToken();
-        t.equal(lexer.peek(), null, 'null token');
+test('lexer.peek', t => {
+    const data = `     name     +     greeting     `;
+    const lexer = Lexer.create(data);
 
-        t.end();
-    });
+    t.equal(lexer.peek().value, 'name', 'peek token');
 
-    test('lexer.peek', t => {
-        const data = `     name     +     greeting     `;
-        const lexer = Lexer.create(data);
+    lexer.nextToken();
+    t.equal(lexer.peek().value, '+', 'peek token');
 
-        t.throws(() => lexer.error('Ya dun'), /Ya dun/, 'lexer throws');
-        t.end();
-    });
+    lexer.nextToken();
+    t.equal(lexer.peek().value, 'greeting', 'peek token');
 
-    test('Lexer - StringLiteral - line endings in string literals error', t => {
-        // This can't be done with error cases because text editors transform line endings
-        t.throws(() => Lexer.all(String.raw`\n`), /LexerError: \[Source]: Unexpected token "\\" \(1:0\)/);
-        t.throws(() => Lexer.all(String.raw`\r`), /LexerError: \[Source]: Unexpected token "\\" \(1:0\)/);
-        t.throws(() => Lexer.all(String.raw`\r\n`), /LexerError: \[Source]: Unexpected token "\\" \(1:0\)/);
-        t.end();
-    });
+    lexer.nextToken();
+    t.equal(lexer.peek(), null, 'null token');
+
+    t.end();
+});
+
+test('lexer.peek', t => {
+    const data = `     name     +     greeting     `;
+    const lexer = Lexer.create(data);
+
+    t.throws(() => lexer.error('Ya dun'), /Ya dun/, 'lexer throws');
+    t.end();
+});
+
+test('Lexer - StringLiteral - line endings in string literals error', t => {
+    // This can't be done with error cases because text editors transform line endings
+    t.throws(() => Lexer.all(String.raw`\n`), /LexerError: \[Source]: Unexpected token "\\" \(1:0\)/);
+    t.throws(() => Lexer.all(String.raw`\r`), /LexerError: \[Source]: Unexpected token "\\" \(1:0\)/);
+    t.throws(() => Lexer.all(String.raw`\r\n`), /LexerError: \[Source]: Unexpected token "\\" \(1:0\)/);
+    t.end();
 });
