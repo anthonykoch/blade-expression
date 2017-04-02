9cfe244414f8c225ff5eac04b159e7927eebdb41
anthonykoch <anthonykoch10@gmail.com>
2017-04-01 18:54:24 -0700

    Fix space size

--- webpack.config.js
+++ webpack.config.js
@@ -4,31 +4,31 @@ const webpack = require('webpack');
 const path    = require('path');
 
 const config = module.exports = {
-    entry: {
-        'app': './lib/index.js',
-    },
-    resolve: {
-        alias: {}
-    },
-    output:
-    {
-        path: './dist',
-        filename: 'bladeexp.js',
-        libraryTarget: 'umd',
-        library: 'Bladexp'
-    },
-    module:
-    {
-        noParse: [],
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            }
-        ],
-    },
-    plugins: [
-        new webpack.optimize.DedupePlugin(),
+  entry: {
+    'app': './lib/index.js',
+  },
+  resolve: {
+    alias: {}
+  },
+  output:
+  {
+    path: './dist',
+    filename: 'bladeexp.js',
+    libraryTarget: 'umd',
+    library: 'Bladexp'
+  },
+  module:
+  {
+    noParse: [],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
     ],
+  },
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+  ],
 };
