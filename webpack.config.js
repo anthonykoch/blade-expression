ce1b5e257934b1a6f336fcca9210bf22afe7b099
anthonykoch <anthonykoch10@gmail.com>
2017-04-01 18:32:09 -0700

    Update scripts and configs

--- webpack.config.js
+++ webpack.config.js
@@ -3,43 +3,32 @@
 const webpack = require('webpack');
 const path    = require('path');
 
-var vendors = {
-	'pad-start':     'pad-start',
-	'object-assign': 'object-assign',
-	'repeat-string': 'repeat-string'
-};
-
 const config = module.exports = {
-	entry: {
-		'app': './lib/index.js',
-	},
-	resolve: {
-		alias: {}
-	},
-	output:
-	{
-		path: './dist',
-		filename: 'bladeexp.js',
-		libraryTarget: 'umd',
-	},
-	module:
-	{
-		noParse: [],
-		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-			}
-		],
-	},
-	plugins: [
-		new webpack.optimize.DedupePlugin(),
-	],
+    entry: {
+        'app': './lib/index.js',
+    },
+    resolve: {
+        alias: {}
+    },
+    output:
+    {
+        path: './dist',
+        filename: 'bladeexp.js',
+        libraryTarget: 'umd',
+        library: 'Bladexp'
+    },
+    module:
+    {
+        noParse: [],
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ],
+    },
+    plugins: [
+        new webpack.optimize.DedupePlugin(),
+    ],
 };
-
-for (var name in vendors) {
-	var file = vendors[name];
-	config.resolve.alias[name] = file;
-	config.module.noParse.push(new RegExp('^' + name + '$'));
-}
